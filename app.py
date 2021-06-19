9143ea4d2dc5cbc683635fa62c200c6b7b59e613
William Hernandez <william@hernandez2.com>
2021-06-19 00:50:03 -0400

    run

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def metrics():
 def hello():
     app.logger.info('Main request successfull')
 
-    return "Hello World!"
+    return "Hello World!!"
 
 if __name__ == "__main__":
     ## stream logs to a file
